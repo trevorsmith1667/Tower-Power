e23f206ff1e731a4c0b11d6a4efd28ece5fc6eb1
Trevor Smith <trevor.smith1667@gmail.com>
2021-03-05 15:57:49 -0800

    working on sprites

--- enemy.js
+++ enemy.js
@@ -1,3 +1,10 @@
+const enemyTypes = [];
+const enemy1 = new Image();
+    enemy1.src = './assets/spritesheet.png';
+    enemyTypes.push(enemy1);
+const enemy2 = new Image();
+    enemy2.src = './assets/angryturtle.png'
+    enemyTypes.push(enemy2);
 
  class Enemy {
     constructor(verticalPosition) {
@@ -9,18 +16,33 @@
         this.movement = this.speed;
         this.health = 100;
         this.maxHealth = this.health;
+        this.enemyType = enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
+        this.frameX = 0;
+        this.frameY = 0;
+        this.minFrame = 0;
+        this.maxFrame = 4;
+        this.spriteWidth = 258;
+        this.spriteHeight = 258;
     }
 
     updateMove() {
         this.x -= this.movement;
+        if (frame% 10 === 0){
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else {
+                this.frameX = this.minFrame;
+            }
+        }
+
     }
 
     draw() {
-        ctx.fillStyle = 'blue'
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+        // ctx.fillStyle = 'blue'
+        // ctx.fillRect(this.x, this.y, this.width, this.height)
         ctx.fillStyle = 'black';
         ctx.font = '20px Arial';
         ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30)
+        ctx.drawImage(this.enemyType, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -34,7 +56,7 @@ function handleEnemies(){
         if (enemies[i].health <= 0){
             let moneyEarned = enemies[i].maxHealth/10;
             meters.push(new Meter('+' + moneyEarned, enemies[i].x, enemies[i].y, 30, 'black'))
-            meters.push(new Meter('+' + powerups[i].amount, 250, 50, 30, 'blue'));
+            meters.push(new Meter('+' + moneyEarned, 250, 50, 30, 'blue'));
             numberOfResources += moneyEarned;
             points += moneyEarned;
             const findIndex = enemyPosition.indexOf(enemies[i].y)
--- tower.js
+++ tower.js
@@ -1,3 +1,7 @@
+const tower1 = new Image();
+    tower1.src = './assets/defender1.png';
+
+
 class Tower {
     constructor(x, y) {
         this.x = x;
@@ -5,9 +9,17 @@ class Tower {
         this.width = cellSize - cellGap * 2;
         this.height = cellSize - cellGap * 2;
         this.shoot = false;
+        this.shooting = false;
         this.health = 100;
-        this.projectiles = [];
+        this.weapons = [];
         this.timer = 0;
+        this.frameX = 0; 
+        this.frameY = 0;
+        this.spriteWidth = 194; 
+        this.spriteHeight = 194;
+        this.minFrame = 0; 
+        this.maxFrame = 16;
+
     }
 
     draw() {
@@ -16,17 +28,21 @@ class Tower {
         ctx.fillStyle = 'black';
         ctx.font = '20px Arial';
         ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30)
+        ctx.drawImage(tower1, this.frameX * this.spriteWidth, 0, this.spriteWidth, 
+            this.spriteHeight, this.x, this.y, this.width, this.height);
     }
     update(){
-        if (this.shoot){
-            this.timer++;
-            if (this.timer % 100 === 0){
-                weapons.push(new Weapon(this.x + 70, this.y + 50));
-            }
-        } else {
-            this.timer = 0
-        };
-    } 
+        //this line will set shoot speed 
+    if (frame % 8 === 0){
+        if (this.frameX < this.maxFrame) this.frameX++;
+        else {this.frameX = this.minFrame}
+        if (this.frameX === 15) this.shooting = true;
+    }
+        if (this.shoot && this.shooting){ 
+            weapons.push(new Weapon(this.x + 70, this.y + 50));  
+            this.shooting = false;   
+        }
+    };
 }
 canvas.addEventListener('click', function () {
     const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
